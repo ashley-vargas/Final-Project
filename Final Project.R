821b8793262f0ec651148ac61557710899b636b0
Ashley Vargas <ashley@Ashleys-MacBook-Pro.local>
2020-10-17 14:57:48 -0600

    Commit

--- Final Project.R
+++ Final Project.R
@@ -3,50 +3,74 @@ library(tidyverse)
 library(rsconnect)
 
 
-mn_contrib <- read.csv("indivs_Minnesota18.csv")
-zip_code_database <- read_csv("zip_code_database.csv")
 
 
-data(county.regions, 
-     package = "choroplethrMaps")
+mn_contrib <- read_csv("~/Desktop/Stat112/indivs_Minnesota18.csv")
+zip_codes <- read_csv("~/Desktop/Stat112/zip_code_database.csv")
 
-region <- county.regions %>%
-  filter(state.name == "minnesota") %>%
-  mutate(county = county.name)
 
-try <- mn_contrib %>%
+states <- map_data("state")
+mn_df <- subset(states, region == "minnesota")
+counties <- map_data("county")
+mn_county <- filter(counties, region == "minnesota") %>%
+  select(-region) %>%
+  mutate(region = subregion)
+
+
+main <- mn_contrib %>%
   mutate(Zip = as.numeric(Zip)) %>%
-  left_join(zip_code_database,
+  left_join(zip_codes,
             by = "Zip") %>%
   mutate(county1 = str_to_lower(county),
-         county2 = str_remove(county1, pattern = " county")) %>%
-  select(-acceptable_cities, -unacceptable_cities, -state, -decommissioned, -country, -world_region, -area_codes, -timezone, -type, -Microfilm, -OtherID, -Type, -Realcode, -Street, -Ultorg, -Contribid, -Cycle, -Fectransid, -Recipcode, -Source, -primary_city, -county, -county1)
+         county2 = str_remove(county1, pattern = " county"),
+         county2 = str_remove(county2, pattern = "\\.")) %>%
+  select(-acceptable_cities, 
+         -unacceptable_cities, 
+         -state, 
+         -decommissioned, 
+         -country, 
+         -world_region, 
+         -area_codes, 
+         -timezone, 
+         -type, 
+         -Microfilm, 
+         -OtherID, 
+         -Type, 
+         -Realcode, 
+         -Street, 
+         -Ultorg, 
+         -Contribid, 
+         -Cycle, 
+         -Recipcode, 
+         -Source, 
+         -primary_city, 
+         -county, 
+         -county1) %>%
+  mutate(county = county2) %>%
+  select(-county2) 
 
 
-region
 
-merged <- try %>%
-  left_join(region, by = c("county2" = "county"))
-
-
-ui <- fluidPage(selectInput("County", 
-                            "County", 
-                            choices = Gender, Occupation, Contrib, Zip, 
-                            multiple = TRUE, 
+ui <- fluidPage(selectInput(inputId = "userchoice1", 
+                            label = "Input Gender Here", 
+                            choices = c("F", "M", "U", "N"),
+                            multiple = FALSE), 
+                numericInput(inputId = "userchoice2", 
+                            label = "Input Zip Code Here", 
+                            value = 55105), 
                 submitButton(text = "Create my plot!"),
                 plotOutput(outputId = "timeplot"))
 
 
-server <- function(input, output) 
-{output$timeplot <- renderPlot({
-  if(length(input$state) > 0) {
-     try %>% 
-      ggplot(aes(x = , y = )) +
-      geom_histogram() +
-      labs(title = " ",
-           y= "",  
-           x = "") +
-      theme_minimal()}})}
+server <- function(input, output){
+  output$timeplot <- renderPlot({
+    mn_contrib %>% 
+      filter(Amount > 0) %>% 
+      filter(Gender == input$userchoice1, Zip == input$userchoice2) %>% 
+      ggplot(aes(Amount)) +
+      geom_density(binwidth = 10) +
+      theme_minimal()})
+    }
 
 
 
